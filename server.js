5d2fabc412e14bdc61dcd3ae10c1fe88aa23d6dd
harasha12 <harshavardhanvangara@outlook.com>
2025-12-08 21:01:12 +0530

    Fix comment formatting in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();   // <-- CREATE APP FIRST
+  // <-- CREATE APP FIRST
 const mysql = require("mysql2");
 const session = require("express-session");
 const flash = require("connect-flash");
